ce0702cd298b9a868e822b7151160af21be29c7a
MadFrank-2006 <maddoxfranklin5@gmail.com>
2025-06-05 18:19:24 -0400

    Set up the basic pages and navbar component in directory
    Pages aren't filled with content though

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import './styles/Home.css';
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+    
+}
+
+export default Home;
